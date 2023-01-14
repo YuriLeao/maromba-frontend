827d9dbe2b7c30fdbd363b95e55a252e3100f3b9
YuriLeao <ydomingosleao@gmail.com>
2023-01-13 21:53:23 -0300

    Ajuste na chamada para fazer login.

--- maromba-web/src/contexts/AuthProvider/util.ts
+++ maromba-web/src/contexts/AuthProvider/util.ts
@@ -1,14 +1,14 @@
 import { api } from "../../services/api";
 import { IUser } from "./type";
 
-export function setUserLocalStorage (user: IUser | null){
+export function setUserLocalStorage(user: IUser | null) {
     localStorage.setItem("u", JSON.stringify(user));
 }
 
-export function getUserLocalStorage () {
+export function getUserLocalStorage() {
     const json = localStorage.getItem("u");
 
-    if(!json) return null;
+    if (!json) return null;
 
     const user = JSON.parse(json);
 
@@ -18,12 +18,18 @@ export function getUserLocalStorage () {
 export async function LoginRequest(email: string, password: string) {
     try {
         const request = await api.get(`/login/${email}/${password}`)
-        .then(response => {
-            return response.data;
-        })
-        .catch(error => {
-            throw error.response.data.mensagem;
-        });
+            .then(response => {
+                return response.data;
+            })
+            .catch(error => {
+                try {
+                    throw error.response.data.mensagem;
+                }
+                catch (errorGenerico) {
+                    throw "Erro ao efetuar login, por favor tente novamente mais tarde";
+                }
+            });
+        return request;
     } catch (error) {
         throw error;
     }
