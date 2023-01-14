472c3b3644d6e853289ffbc34aafa2a235a48233
YuriLeao <ydomingosleao@gmail.com>
2023-01-13 21:53:04 -0300

    Troca na rota de inicial para menu e adição da rota user como rota protegida

--- maromba-web/src/routes/RoutesApp.tsx
+++ maromba-web/src/routes/RoutesApp.tsx
@@ -7,6 +7,7 @@ import { ProtectedRoutes } from "../components/ProtectedRoute";
 import { AuthProvider } from "../contexts/AuthProvider";
 import { Login } from "../pages/Login/Login";
 import { Menu } from "../pages/Menu/Menu";
+import { User } from "../pages/User/User";
 
 export function RoutesApp() {
     return (
@@ -14,11 +15,16 @@ export function RoutesApp() {
             <Router>
                 <Routes>
                     <Route path="/" element={<Login />} />
-                    <Route path="/inicial" element={
+                    <Route path="/menu" element={
                         <ProtectedRoutes >
                             <Menu />
                         </ProtectedRoutes>
                     } >
+                        <Route path="/menu/user" element={
+                            <ProtectedRoutes >
+                                <User />
+                            </ProtectedRoutes>
+                        } ></Route>
                     </Route>
                     <Route path="*" element={<Login />} />
                 </Routes>
