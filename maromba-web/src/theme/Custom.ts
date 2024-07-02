3d481e76c8e0073ae15516b048ef227dcf2733db
YuriLeao <ydomingosleao@gmail.com>
2024-07-02 16:30:16 -0300

    Teste para usar a lib Mui de componentes
    salvando thema do input e autocomplete

---
+++ maromba-web/src/theme/Custom.ts
@@ -0,0 +1,62 @@
+/*import { createTheme } from "@mui/material";
+
+export const customTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: "#700B97;",
+            light: "#8E05C2",
+            dark: "#3E065F",
+            contrastText: "#f9f9f9",
+        },
+    },
+    components: {
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    // Standard
+                    width: '100%',
+                    "& .MuiInput-input": {
+                    },
+                    "& .MuiInput-root": {
+                        fontFamily: "Sora",
+                        fontSize: "18px",
+                        "&:before": {
+                            borderBottom: "2px solid #f9f9f9",
+                        },
+                        "&:after": {
+                            borderWidth: "2px",
+                        },
+                        ":hover:not(.Mui-focused)": {
+                            "&:before": {
+                                borderWidth: "2px",
+                            },
+                        },
+                    },
+                    "& .MuiInputLabel-root.Mui-focused": {
+                        color: "#f9f9f9",
+                    },
+                    "& .MuiFormLabel-root": {
+                        fontFamily: "Sora",
+                        fontSize: "16px",
+                        color: "#f9f9f9",
+                    },
+                    "& .MuiInputLabel-shrink": {
+                        transform: "translate(0, -1.5px) scale(0.875)",
+                    },
+                    "& .MuiInputLabel-root.Mui-error": {
+                        color: "#f9f9f9",
+                    },
+                    "& .Mui-error::before": {
+                        borderBottomColor: "red !important",
+                    },
+                    "& .Mui-error::after": {
+                        animation: "shake 0.1635s 0s 3",
+                        borderBottomColor: "red !important",
+                    }
+                    
+                },
+            },
+        },
+    },
+});*/
