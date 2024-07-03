2d58394e8e9e2814fafe4a01a3584b4bbd501965
YuriLeao <ydomingosleao@gmail.com>
2024-07-03 01:00:50 -0300

    Modificação para usar a lista do select personalisada

--- maromba-web/src/components/Select/Select.tsx
+++ maromba-web/src/components/Select/Select.tsx
@@ -1,37 +1,36 @@
-import React, { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Select.css";
 
 interface Props {
     register: any;
     label: string;
-    icon?: string;
     name: string;
     erro: boolean;
     list: Item[];
     required?: boolean;
 }
 
+
 interface Item {
     id: string,
     name: string
 }
 
-export const Select = (({ register, label, icon, name, erro, list, required }: Props) => {
-    const [value, setValue] = useState('');
+export const Select = (({ register, label, name, erro, required, list }: Props) => {
+    const [inputValue, setInputValue] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
+    const inputRef = useRef<HTMLDivElement>(null);
+    const listRef = useRef<HTMLDivElement>(null);
 
-    let classes: string = "";
-    let divClasses: string = "textbox";
+    let classes: string = "select";
+    let divClasses: string = "textbox unIncon";
     let paramRegister = { required: false };
 
     if (required == true) {
         paramRegister.required = true;
     }
 
-    if (icon == undefined) {
-        divClasses += " unIncon";
-    }
-
-    if (value) {
+    if (inputValue) {
         classes += " has-value";
     }
 
@@ -39,33 +38,110 @@ export const Select = (({ register, label, icon, name, erro, list, required }: P
         classes += " invalid";
     }
 
+    const handleSelectClick = (e: any) => {
+        e.stopPropagation();
+        setIsOpen(true);
+    };
+
+    function handleClickList(value: Item) {
+        setInputValue(value.name);
+        setIsOpen(false);
+    };
+
+    function updateOptionsPosition() {
+        if (listRef.current != null && inputRef.current != null) {
+            var inputRect = inputRef.current.getBoundingClientRect();
+            var optionsHeight = listRef.current.offsetHeight;
+            var viewportHeight = window.innerHeight;
+            if (viewportHeight - inputRect.bottom >= optionsHeight) {
+                listRef.current.style.top = inputRect.bottom + 'px';
+            } else {
+                listRef.current.style.top = (inputRect.top - listRef.current.offsetHeight - 15) + 'px';
+            }
+            listRef.current.style.left = inputRect.left + 'px';
+            listRef.current.style.width = inputRect.width + 'px';
+        }
+    }
+
+    const handleClickOutside = (e: any) => {
+        if (inputRef.current &&
+            !inputRef.current.contains(e.target) &&
+            listRef.current &&
+            !listRef.current.contains(e.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        if (inputRef.current != null && inputRef.current.parentElement != null
+            && inputRef.current.parentElement.parentElement != null) {
+            if (isOpen) {
+                updateOptionsPosition();
+                inputRef.current.parentElement.parentElement.addEventListener('scroll', updateOptionsPosition);
+                inputRef.current.parentElement.parentElement.addEventListener('resize', updateOptionsPosition);
+                window.addEventListener('scroll', updateOptionsPosition);
+                window.addEventListener('resize', updateOptionsPosition);
+                document.addEventListener('click', handleClickOutside);
+            } else {
+                inputRef.current.parentElement.parentElement.removeEventListener('scroll', updateOptionsPosition);
+                inputRef.current.parentElement.parentElement.removeEventListener('resize', updateOptionsPosition);
+                window.removeEventListener('scroll', updateOptionsPosition);
+                window.removeEventListener('resize', updateOptionsPosition);
+                document.removeEventListener('click', handleClickOutside);
+            }
+        }
+
+        return () => {
+            if (inputRef.current != null && inputRef.current.parentElement != null
+                && inputRef.current.parentElement.parentElement != null) {
+                inputRef.current.parentElement.parentElement.removeEventListener('scroll', updateOptionsPosition);
+                inputRef.current.parentElement.parentElement.removeEventListener('resize', updateOptionsPosition);
+            }
+            window.removeEventListener('scroll', updateOptionsPosition);
+            window.removeEventListener('resize', updateOptionsPosition);
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [isOpen]);
+
     return (
         <div className={divClasses}>
-            <select
+            <input
                 id={name}
                 name={name}
+                type="text"
+                step="any"
                 {...register(name, paramRegister)}
-                value={value}
-                onChange={(e) => { setValue(e.target.value); }}
-                style={(icon == undefined) ? { padding: '0px' } : { padding: '0px 0px 0px 50px' }}
-                className={classes} >
-                    <option style={{display: "none"}}></option>
-                {list.map((item, index) => (
-                    (<React.Fragment key={item.id}>
-                        <option value={item.id}>{item.name}</option>
-                    </React.Fragment>)
-                ))}
-            </select>
-            <span style={(icon == undefined) ? { display: 'none' } : { display: 'inline-block' }}
-                className='material-symbols-outlined'>
-                {icon}
-            </span>
+                style={{ padding: '0px 0px 0px 5px' }}
+                className={classes}
+                value={inputValue}
+                ref={inputRef}
+                onClick={handleSelectClick} />
             <label
                 htmlFor={name}
-                style={(icon == undefined) ? { left: '0px' } : { left: '50px' }}
-                className={value ? 'roxo' : ''}>
+                style={{ left: '0px' }}
+                className={'select'}>
                 {label}
             </label>
+            <span style={{ display: 'inline-block', right: '-1%', left: 'unset', cursor: 'pointer' }}
+                className='material-symbols-outlined'>
+                {"keyboard_arrow_down"}
+            </span>
+
+            {isOpen &&
+                <div className="listResult" ref={listRef} >
+                    <div className="listResultScroll" >
+                        {
+                            list.map(value => {
+                                return <div key={value.id} className='listItem' onClick={() => handleClickList(value)}>
+                                    <p className="ml5">{value.name}</p>
+                                </div>
+                            })
+                        }
+                    </div>
+                </div>
+            }
         </div>
+
+
     );
 });
